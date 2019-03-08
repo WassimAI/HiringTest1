65e7575dc3394bfeb4a2016980d311c36fbd30f2
wassimAI - Wassim Ibrahim <40058827+WassimInstaclass@users.noreply.github.com>
2019-03-08 19:47:52 +0400

    Add project files.

---
+++ HiringTest1/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="HiringTest1.MvcApplication" Language="C#" %>
---
+++ HiringTest1/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(HiringTest1.Startup))]
+namespace HiringTest1
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
